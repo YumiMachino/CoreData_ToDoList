9b323faee50b250a588ead70dbfaf2ec5bdb39b1
YumiMachino <y.y.1515m@gmail.com>
2021-02-21 14:18:06 -0800

    Initial Commit

---
+++ CoreData_ToDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData_ToDo
+//
+//  Created by Yumi Machino on 2021/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
